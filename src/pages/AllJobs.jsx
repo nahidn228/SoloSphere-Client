1733d42758aa43ff2cc82a8495fe0cacd5a0fc03
Nahid Hasan <nahidn228@gmail.com>
2024-12-20 23:01:00 +0600

    change search box onChange to onBlur

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -45,12 +45,16 @@ const AllJobs = () => {
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
                 name="search"
-                onChange={(e) => setSearch(e.target.value)}
+                // onChange={(e) => setSearch(e.target.value)}
+                onBlur={(e) => setSearch(e.target.value)}
                 placeholder="Enter Job Title"
                 aria-label="Enter Job Title"
               />
 
-              <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
+              <button
+                type="button"
+                className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none"
+              >
                 Search
               </button>
             </div>
