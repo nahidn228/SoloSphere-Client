7548b4bb4923ccebdb31dbf1aa586d7176c8b88a
Nahid Hasan <nahidn228@gmail.com>
2024-12-20 10:23:32 +0600

    bid form validation done

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { format } from "date-fns";
-import { useEffect, useState } from "react";
+import { compareAsc, format } from "date-fns";
+import { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../providers/AuthProvider";
 
 const JobDetails = () => {
+  const { user } = useContext(AuthContext);
   const { id } = useParams();
   const [startDate, setStartDate] = useState(new Date());
 
@@ -34,6 +37,36 @@ const JobDetails = () => {
     description,
     bid_count,
   } = job || {};
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const price = form.price.value;
+    const email = form.email.value;
+    const comment = form.comment.value;
+    const date = startDate;
+
+    //check bid permission validation
+    if (user?.email === buyer?.email)
+      return toast.error("Action Not permitted");
+
+    //Deadline cross validation for bidding date
+    const compareDate = compareAsc(new Date(date), new Date(deadline));
+    if (compareDate === 1) {
+      return toast.error("Offer a date within deadline");
+    }
+    //Deadline cross validation for running date
+    const compareDate1 = compareAsc(new Date(), new Date(deadline));
+    if (compareDate1 === 1)
+      return toast.error("Deadline crossed, Bidding error");
+
+    // maximum price validation
+    if (price > max_price)
+      return toast.error("Offer less or at lest equal to  maximum price!");
+
+    const bidData = { price, email, comment, date };
+    console.table(bidData);
+  };
 
   return (
     <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto ">
@@ -69,7 +102,7 @@ const JobDetails = () => {
               </p>
             </div>
             <div className="rounded-full object-cover overflow-hidden w-14 h-14">
-              <img src={buyer?.photo} alt="" />
+              <img referrerPolicy="no-referrer" src={buyer?.photo} alt="" />
             </div>
           </div>
           <p className="mt-6 text-lg font-bold text-gray-600 ">
@@ -77,13 +110,14 @@ const JobDetails = () => {
           </p>
         </div>
       </div>
+
       {/* Place A Bid Form */}
       <section className="p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]">
         <h2 className="text-lg font-semibold text-gray-700 capitalize ">
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="price">
@@ -106,6 +140,7 @@ const JobDetails = () => {
                 id="emailAddress"
                 type="email"
                 name="email"
+                defaultValue={user?.email}
                 disabled
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
